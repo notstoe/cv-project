7170d2517959a87032f6cccf465154da98da687d
Gustavo <gstonin95@gmail.com>
2020-10-26 20:28:01 +0000

    Updates

---
+++ precache-manifest.63bfb53e2c50f47c9e71a484c7fc9244.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75213dbc5c6f849846b66cd29f74cf01",
+    "url": "/cv-project/index.html"
+  },
+  {
+    "revision": "1a6ce382ecf85db9cb8e",
+    "url": "/cv-project/static/css/main.36c6bcf4.chunk.css"
+  },
+  {
+    "revision": "0b6e00e8fea16fdedde5",
+    "url": "/cv-project/static/js/2.1f48f13b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv-project/static/js/2.1f48f13b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6ce382ecf85db9cb8e",
+    "url": "/cv-project/static/js/main.05ef3261.chunk.js"
+  },
+  {
+    "revision": "f1ebb3293288666c5950",
+    "url": "/cv-project/static/js/runtime-main.d91c792d.js"
+  },
+  {
+    "revision": "02e85c278e276802a0406e11287a976a",
+    "url": "/cv-project/static/media/github.02e85c27.png"
+  }
+]);
